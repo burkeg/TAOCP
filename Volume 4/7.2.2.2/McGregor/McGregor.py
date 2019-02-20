0a0cde77682d673b470ef9311871ec4a45f31d75
BURKE,GABE (K-SantaRosa,ex1) <gabe.burke@keysight.com>
2019-02-20 09:39:57 -0800

    finished constructing the list of edges to each vertex. Verified n=3,4 by hand. I used 2 different methods to verify n=10. Method 1: sum up counts of edges to each vertex then divide by two. Method 2: for each vertex, insert the sorted pair of vertices connected by each edge into a hashset, then count the size of that set. The only way a mistake can slip through the cracks with this method is if a node mistakenly connects to the wrong node, and that other node mistakenly connects to the first node. Both methods agree at 324 edges for n=10.

--- Volume 4/7.2.2.2/McGregor/McGregor.py
+++ Volume 4/7.2.2.2/McGregor/McGregor.py
@@ -16,58 +16,171 @@ class McGregor:
 
 
     def populateConnections(self):
+        n=self.n
         for node in self.nodeDict.keys():
+            i = node[0]
+            j = node[1]
             # Handle row 0
-            if node[0] is 0:
-                if node[1] is 0:
+            if i is n-1 and j is n-1:
+                    self.nodeDict[node].add((2,1))    #node to bottom right
+                    self.nodeDict[node].add((2,0))    #node to bottom left
+                    self.nodeDict[node].add((n-2,n-1))    #node to top right
+                    self.nodeDict[node].add((n-2,n-2))    #node to top left
+            elif i is 0:
+                if j is 0:
                     self.nodeDict[node].add((0,1))    #node to right
                     self.nodeDict[node].add((1,1))    #node to bottom right
-                    self.nodeDict[node].add((self.n,self.n-1))    #node above
-                    for index in range(math.floor(self.n/2)+1):
-                        self.nodeDict[node].add((self.n,index))    #nodes across bottom row
-                elif node[1] is self.n-1:
-                    self.nodeDict[node].add((node[0],node[1]-1))    #node to left
-                    self.nodeDict[node].add((node[0]+1,node[1]))    #node to bottom left
-                    self.nodeDict[node].add((self.n,self.n-1))    #node surrounding
+                    self.nodeDict[node].add((1,0))    #node to lower bottom right
+                    self.nodeDict[node].add((n,n-1))    #node above
+                    for index in range(math.floor(n/2)+1):
+                        self.nodeDict[node].add((n,index))    #nodes across bottom row
+                elif j is n-1:
+                    self.nodeDict[node].add((i,j-1))    #node to left
+                    self.nodeDict[node].add((i+1,j))    #node to bottom left
+                    self.nodeDict[node].add((n,n-1))    #node surrounding
                 else:
-                    self.nodeDict[node].add((self.n,self.n-1))    #node above
-                    self.nodeDict[node].add((node[0],node[1]-1))    #node to left
-                    self.nodeDict[node].add((node[0],node[1]+1))    #node to right
-                    self.nodeDict[node].add((node[0]+1,node[1]))    #node to bottom left
-                    self.nodeDict[node].add((node[0]+1,node[1]+1))    #node to bottom right
+                    self.nodeDict[node].add((n,n-1))    #node above
+                    self.nodeDict[node].add((i,j-1))    #node to left
+                    self.nodeDict[node].add((i,j+1))    #node to right
+                    self.nodeDict[node].add((i+1,j))    #node to bottom left
+                    self.nodeDict[node].add((i+1,j+1))    #node to bottom right
             # Handle row 1
-            elif node[0] is 1:
-                if node[1] is 0:
+            elif i is 1:
+                if j is 0:
                     self.nodeDict[node].add((0,0))    #node to left
-                    for index in range(math.floor(self.n/2), self.n):
-                        self.nodeDict[node].add((self.n,index))    #nodes across bottom row
-                elif node[1] is 1:
+                    for index in range(math.floor(n/2), n):
+                        self.nodeDict[node].add((n,index))    #nodes across bottom row
+                elif j is 1:
                     self.nodeDict[node].add((0,0))    #node to left
-                    self.nodeDict[node].add((node[0]-1,node[1]))    #node above
-                    self.nodeDict[node].add((self.n,0))    #node to lower bottom left
-                    self.nodeDict[node].add((self.n-1,0))    #node to lower bottom right
-                    self.nodeDict[node].add((node[0]+1,node[1]+1))    #node to upper bottom right
-                    self.nodeDict[node].add((node[0],node[1]+1))    #node to right
-                elif node[1] is self.n-1:
-                    self.nodeDict[node].add((node[0]+1,node[1]))    #node to bottom left
-                    self.nodeDict[node].add((node[0],node[1]-1))    #node to left
-                    self.nodeDict[node].add((node[0]-1,node[1]-1))    #node to upper left
-                    self.nodeDict[node].add((node[0]-1,node[1]))    #node to upper right
-                    self.nodeDict[node].add((self.n-node[0]+1,node[1]-(node[0]-1)))    #node to lower bottom right
-                    self.nodeDict[node].add((self.n-node[0], node[1]-(node[0]-1)-1))    #node to lower bottom left
+                    self.nodeDict[node].add((i-1,j))    #node above
+                    self.nodeDict[node].add((n,0))    #node to lower bottom left
+                    self.nodeDict[node].add((n-1,0))    #node to lower bottom right
+                    self.nodeDict[node].add((i+1,j+1))    #node to upper bottom right
+                    self.nodeDict[node].add((i,j+1))    #node to right
+                elif j is n-1:
+                    self.nodeDict[node].add((i+1,j))    #node to bottom left
+                    self.nodeDict[node].add((i,j-1))    #node to left
+                    self.nodeDict[node].add((i-1,j-1))    #node to upper left
+                    self.nodeDict[node].add((i-1,j))    #node to upper right
+                    self.nodeDict[node].add((n-i+1,j-(i-1)))    #node to lower bottom right
+                    self.nodeDict[node].add((n-i, j-(i-1)-1))    #node to lower bottom left
                 else:
-                    self.nodeDict[node].add((node[0]-1,node[1]-1))    #node to upper left
-                    self.nodeDict[node].add((node[0]-1,node[1]))    #node to upper right
-                    self.nodeDict[node].add((node[0],node[1]-1))    #node to left
-                    self.nodeDict[node].add((node[0],node[1]+1))    #node to right
-                    self.nodeDict[node].add((node[0]+1,node[1]))    #node to bottom left
-                    self.nodeDict[node].add((node[0]+1,node[1]+1))    #node to bottom right
+                    self.nodeDict[node].add((i-1,j-1))    #node to upper left
+                    self.nodeDict[node].add((i-1,j))    #node to upper right
+                    self.nodeDict[node].add((i,j-1))    #node to left
+                    self.nodeDict[node].add((i,j+1))    #node to right
+                    self.nodeDict[node].add((i+1,j))    #node to bottom left
+                    self.nodeDict[node].add((i+1,j+1))    #node to bottom right
             # Handle row 2
-            elif node[0] is 2:
-                pass
-
+            elif i is 2:
+                if j is 0:
+                    self.nodeDict[node].add((n-1,n-1))    #node to upper right
+                    self.nodeDict[node].add((2,1))    #node to right
+                    self.nodeDict[node].add((3,1))    #node to bottom right
+                    self.nodeDict[node].add((3,0))    #node to bottom left
+                    self.nodeDict[node].add((n-2,n-2))    #node above
+                elif j is 1:
+                    self.nodeDict[node].add((n-1,n-1))    #node to upper left
+                    self.nodeDict[node].add((2,0))    #node to left
+                    self.nodeDict[node].add((3,2))    #node to bottom right
+                    self.nodeDict[node].add((3,1))    #node to bottom left
+                    self.nodeDict[node].add((n-2,n-1))    #node above
+                elif j is 2:
+                    self.nodeDict[node].add((1,1))    #node to upper left
+                    self.nodeDict[node].add((1,2))    #node to upper right
+                    self.nodeDict[node].add((2,3))    #node to right
+                    self.nodeDict[node].add((3,3))    #node to upper bottom right
+                    self.nodeDict[node].add((n-j,0))    #node to lower bottom right
+                    self.nodeDict[node].add((n-j+1,0))    #node to lower bottom left
+                elif j is n-1:
+                    self.nodeDict[node].add((i-1,j-1))    #node to upper left
+                    self.nodeDict[node].add((i-1,j))    #node to upper right
+                    self.nodeDict[node].add((i,j-1))    #node to left
+                    self.nodeDict[node].add((i+1,j))    #node to upper bottom left
+                    self.nodeDict[node].add((n-i,n-i-1))    #node to lower bottom left
+                    self.nodeDict[node].add((n-i+1,n-i))    #node to lower bottom right
+                else:
+                    self.nodeDict[node].add((i-1,j-1))    #node to upper left
+                    self.nodeDict[node].add((i-1,j))    #node to upper right
+                    self.nodeDict[node].add((i,j-1))    #node to left
+                    self.nodeDict[node].add((i,j+1))    #node to right
+                    self.nodeDict[node].add((i+1,j))    #node to bottom left
+                    self.nodeDict[node].add((i+1,j+1))    #node to bottom right
+            elif i is n:
+                if j is 0:
+                    self.nodeDict[node].add((0,0))    #node to upper left
+                    self.nodeDict[node].add((i-1,j))    #node to bottom upper right
+                    self.nodeDict[node].add((i,j+1))    #node to right
+                    self.nodeDict[node].add((1,1))    #node to top upper right
+                elif j is n-1:
+                    self.nodeDict[node].add((1,0))    #node to bottom
+                    self.nodeDict[node].add((i,j-1))    #node to left
+                    self.nodeDict[node].add((i-1,j-1))    #node to lower top left
+                    self.nodeDict[node].add((1,n-1))    #node to middle top left
+                    for index in range(n):
+                        self.nodeDict[node].add((0,index))    #nodes across top row
+                else:
+                    if j <= math.floor(n/2):                   #node left of middle on bottom row
+                        self.nodeDict[node].add((0,0))
+                    if j >= math.floor(n/2):                   #node right of middle on bottom row
+                        self.nodeDict[node].add((1,0))
+                    self.nodeDict[node].add((i-1,j-1))    #node to upper left
+                    self.nodeDict[node].add((i-1,j))    #node to upper right
+                    self.nodeDict[node].add((i,j-1))    #node to left
+                    self.nodeDict[node].add((i,j+1))    #node to right
+            else:
+                if i is j:
+                    self.nodeDict[node].add((i-1,j-1))    #node to upper left
+                    self.nodeDict[node].add((i-1,j))    #node to upper right
+                    self.nodeDict[node].add((i,j+1))    #node to right
+                    self.nodeDict[node].add((i+1,j+1))    #node to upper bottom right
+                    self.nodeDict[node].add((n-i,0))    #node to lower bottom right
+                    self.nodeDict[node].add((n-i+1,0))    #node to lower bottom left
+                elif j is n-1:
+                    self.nodeDict[node].add((i-1,j-1))    #node to upper left
+                    self.nodeDict[node].add((i-1,j))    #node to upper right
+                    self.nodeDict[node].add((i,j-1))    #node to left
+                    self.nodeDict[node].add((i+1,j))    #node to upper bottom left
+                    self.nodeDict[node].add((n-i,n-i-1))    #node to lower bottom left
+                    self.nodeDict[node].add((n-i+1,n-i))    #node to lower bottom right
+                elif j is 0:
+                    self.nodeDict[node].add((n-i,n-i))    #node to upper left
+                    self.nodeDict[node].add((i-1,j))    #node to bottom upper right
+                    self.nodeDict[node].add((i,j+1))    #node to right
+                    self.nodeDict[node].add((n-i+1,n-i+1))    #node to top upper right
+                    self.nodeDict[node].add((i+1,j+1))    #node to bottom right
+                    self.nodeDict[node].add((i+1,j))    #node to bottom left
+                elif j is i - 1:
+                    self.nodeDict[node].add((i+1,j))    #node to bottom left
+                    self.nodeDict[node].add((i+1,j+1))    #node to bottom right
+                    self.nodeDict[node].add((i,j-1))    #node to left
+                    self.nodeDict[node].add((i-1,j-1))    #node to lower top left
+                    self.nodeDict[node].add((n-i,n-1))    #node to upper top right
+                    self.nodeDict[node].add((n-i+1,n-1))    #node to upper top left
+                else:
+                    self.nodeDict[node].add((i-1,j-1))    #node to upper left
+                    self.nodeDict[node].add((i-1,j))    #node to upper right
+                    self.nodeDict[node].add((i,j-1))    #node to left
+                    self.nodeDict[node].add((i,j+1))    #node to right
+                    self.nodeDict[node].add((i+1,j))    #node to bottom left
+                    self.nodeDict[node].add((i+1,j+1))    #node to bottom right
+        for k, v in self.nodeDict.items():
+            self.nodeDict[k]=sorted(v)
 
 
 if __name__ == "__main__":
     #solve('small_instances.txt')
-    mg = McGregor(4)
+    n=10
+    mg = McGregor(n)
+    regions = 0
+    edges = 0
+    edgesSoFar = set()
+    for k, v in mg.nodeDict.items():
+        regions+=1
+        edges+=len(v)
+        for edge in v:
+            edgesSoFar.add(frozenset([k[0]*(n+1)+k[1], edge[0]*(n+1)+edge[1]]))
+    edgesSoFar = [(math.floor(tuple(x)[0]/(n+1)),tuple(x)[0]%(n+1)) for x in edgesSoFar]
+    pp.pprint(sorted(edgesSoFar))
+    print(len(edgesSoFar))
+    print(int(edges/2))
