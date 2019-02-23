4c7e04725fc18c0525997b9feb0a4a71b995c33e
BURKE,GABE (K-SantaRosa,ex1) <gabe.burke@keysight.com>
2019-02-22 19:54:48 -0800

    Added a utility class for manipulating SAT clauses. Given a set of literals, I can generate a set of clauses that is satisfiable when <= == or >= r literals are assigned true. I also added some support for remapping expressions into equivalent ones covering a different set of integers. I expect that will be useful when trying to allocate space for many sets of auxiliary literals.

--- Volume 4/7.2.2.2/McGregor/McGregor.py
+++ Volume 4/7.2.2.2/McGregor/McGregor.py
@@ -2,7 +2,9 @@ import pycosat
 import math
 import numpy as np
 import pprint as pp
+import sys
 import collections
+
 class McGregor:
     def  __init__(self, n, d, all=False):
         if (n < 3):
@@ -24,6 +26,7 @@ class McGregor:
             for j in range(n):
                 self.nodeToClauseDict[(i,j)] = i*n+j
         self.clauses = []
+        self.genClauses()
 
     def populateConnections(self):
         n=self.n
@@ -31,20 +34,20 @@ class McGregor:
             i = node[0]
             j = node[1]
             # Handle row 0
-            if i is n-1 and j is n-1:
+            if i == n-1 and j == n-1:
                     self.nodeDict[node].add((2,1))    #node to bottom right
                     self.nodeDict[node].add((2,0))    #node to bottom left
                     self.nodeDict[node].add((n-2,n-1))    #node to top right
                     self.nodeDict[node].add((n-2,n-2))    #node to top left
-            elif i is 0:
-                if j is 0:
+            elif i == 0:
+                if j == 0:
                     self.nodeDict[node].add((0,1))    #node to right
                     self.nodeDict[node].add((1,1))    #node to bottom right
                     self.nodeDict[node].add((1,0))    #node to lower bottom right
                     self.nodeDict[node].add((n,n-1))    #node above
                     for index in range(math.floor(n/2)+1):
                         self.nodeDict[node].add((n,index))    #nodes across bottom row
-                elif j is n-1:
+                elif j == n-1:
                     self.nodeDict[node].add((i,j-1))    #node to left
                     self.nodeDict[node].add((i+1,j))    #node to bottom left
                     self.nodeDict[node].add((n,n-1))    #node surrounding
@@ -55,19 +58,19 @@ class McGregor:
                     self.nodeDict[node].add((i+1,j))    #node to bottom left
                     self.nodeDict[node].add((i+1,j+1))    #node to bottom right
             # Handle row 1
-            elif i is 1:
-                if j is 0:
+            elif i == 1:
+                if j == 0:
                     self.nodeDict[node].add((0,0))    #node to left
                     for index in range(math.floor(n/2), n):
                         self.nodeDict[node].add((n,index))    #nodes across bottom row
-                elif j is 1:
+                elif j == 1:
                     self.nodeDict[node].add((0,0))    #node to left
                     self.nodeDict[node].add((i-1,j))    #node above
                     self.nodeDict[node].add((n,0))    #node to lower bottom left
                     self.nodeDict[node].add((n-1,0))    #node to lower bottom right
                     self.nodeDict[node].add((i+1,j+1))    #node to upper bottom right
                     self.nodeDict[node].add((i,j+1))    #node to right
-                elif j is n-1:
+                elif j == n-1:
                     self.nodeDict[node].add((i+1,j))    #node to bottom left
                     self.nodeDict[node].add((i,j-1))    #node to left
                     self.nodeDict[node].add((i-1,j-1))    #node to upper left
@@ -82,27 +85,27 @@ class McGregor:
                     self.nodeDict[node].add((i+1,j))    #node to bottom left
                     self.nodeDict[node].add((i+1,j+1))    #node to bottom right
             # Handle row 2
-            elif i is 2:
-                if j is 0:
+            elif i == 2:
+                if j == 0:
                     self.nodeDict[node].add((n-1,n-1))    #node to upper right
                     self.nodeDict[node].add((2,1))    #node to right
                     self.nodeDict[node].add((3,1))    #node to bottom right
                     self.nodeDict[node].add((3,0))    #node to bottom left
                     self.nodeDict[node].add((n-2,n-2))    #node above
-                elif j is 1:
+                elif j == 1:
                     self.nodeDict[node].add((n-1,n-1))    #node to upper left
                     self.nodeDict[node].add((2,0))    #node to left
                     self.nodeDict[node].add((3,2))    #node to bottom right
                     self.nodeDict[node].add((3,1))    #node to bottom left
                     self.nodeDict[node].add((n-2,n-1))    #node above
-                elif j is 2:
+                elif j == 2:
                     self.nodeDict[node].add((1,1))    #node to upper left
                     self.nodeDict[node].add((1,2))    #node to upper right
                     self.nodeDict[node].add((2,3))    #node to right
                     self.nodeDict[node].add((3,3))    #node to upper bottom right
                     self.nodeDict[node].add((n-j,0))    #node to lower bottom right
                     self.nodeDict[node].add((n-j+1,0))    #node to lower bottom left
-                elif j is n-1:
+                elif j == n-1:
                     self.nodeDict[node].add((i-1,j-1))    #node to upper left
                     self.nodeDict[node].add((i-1,j))    #node to upper right
                     self.nodeDict[node].add((i,j-1))    #node to left
@@ -116,13 +119,13 @@ class McGregor:
                     self.nodeDict[node].add((i,j+1))    #node to right
                     self.nodeDict[node].add((i+1,j))    #node to bottom left
                     self.nodeDict[node].add((i+1,j+1))    #node to bottom right
-            elif i is n:
-                if j is 0:
+            elif i == n:
+                if j == 0:
                     self.nodeDict[node].add((0,0))    #node to upper left
                     self.nodeDict[node].add((i-1,j))    #node to bottom upper right
                     self.nodeDict[node].add((i,j+1))    #node to right
                     self.nodeDict[node].add((1,1))    #node to top upper right
-                elif j is n-1:
+                elif j == n-1:
                     self.nodeDict[node].add((1,0))    #node to bottom
                     self.nodeDict[node].add((i,j-1))    #node to left
                     self.nodeDict[node].add((i-1,j-1))    #node to lower top left
@@ -139,28 +142,28 @@ class McGregor:
                     self.nodeDict[node].add((i,j-1))    #node to left
                     self.nodeDict[node].add((i,j+1))    #node to right
             else:
-                if i is j:
+                if i == j:
                     self.nodeDict[node].add((i-1,j-1))    #node to upper left
                     self.nodeDict[node].add((i-1,j))    #node to upper right
                     self.nodeDict[node].add((i,j+1))    #node to right
                     self.nodeDict[node].add((i+1,j+1))    #node to upper bottom right
                     self.nodeDict[node].add((n-i,0))    #node to lower bottom right
                     self.nodeDict[node].add((n-i+1,0))    #node to lower bottom left
-                elif j is n-1:
+                elif j == n-1:
                     self.nodeDict[node].add((i-1,j-1))    #node to upper left
                     self.nodeDict[node].add((i-1,j))    #node to upper right
                     self.nodeDict[node].add((i,j-1))    #node to left
                     self.nodeDict[node].add((i+1,j))    #node to upper bottom left
                     self.nodeDict[node].add((n-i,n-i-1))    #node to lower bottom left
                     self.nodeDict[node].add((n-i+1,n-i))    #node to lower bottom right
-                elif j is 0:
+                elif j == 0:
                     self.nodeDict[node].add((n-i,n-i))    #node to upper left
                     self.nodeDict[node].add((i-1,j))    #node to bottom upper right
                     self.nodeDict[node].add((i,j+1))    #node to right
                     self.nodeDict[node].add((n-i+1,n-i+1))    #node to top upper right
                     self.nodeDict[node].add((i+1,j+1))    #node to bottom right
                     self.nodeDict[node].add((i+1,j))    #node to bottom left
-                elif j is i - 1:
+                elif j == i - 1:
                     self.nodeDict[node].add((i+1,j))    #node to bottom left
                     self.nodeDict[node].add((i+1,j+1))    #node to bottom right
                     self.nodeDict[node].add((i,j-1))    #node to left
@@ -224,13 +227,15 @@ class McGregor:
                 edgesSoFar.add(frozenset([k[0]*(n+1)+k[1], edge[0]*(n+1)+edge[1]]))
         edgesSoFar = [(math.floor(tuple(x)[0]/(n+1)),tuple(x)[0]%(n+1)) for x in edgesSoFar]
         # check that number of edges is correct
-        if int(edges/2) is not 3*N-6:
+        if int(edges/2) != int(3*N-6):
+            print(1, int(edges/2), int(3*N-6))
             return False
         # check that number of unique edges is correct
-        elif len(edgesSoFar) is not 3*N-6:
+        elif len(edgesSoFar) != int(3*N-6):
+            print(2, len(edgesSoFar), int(3*N-6))
             return False
         # check that number of vertices is correct
-        elif regions is not N:
+        elif regions != N:
             return False
         # check that adjacency matrix is symmetric
         for i in range(n+1):
@@ -240,7 +245,6 @@ class McGregor:
         return True
 
     def solve(self):
-        self.genClauses()
         self.assignments = pycosat.solve(self.clauses)
         if self.assignments == 'UNSAT':
             return False
@@ -258,17 +262,139 @@ class McGregor:
                     validColors[self.getNode(literal)].add(self.getColor(literal))
         return validColors
 
+    def testLimits(self):
+        originalClauses = self.clauses
+        
+
+class SATUtils:
+    # applies a mapping from one literal to another
+    @staticmethod
+    def applyLiteralMapping(clauses, mapping, oldToNew=True):
+        tmp_map = mapping
+        if oldToNew:
+            tmp_map = {v: k for k, v in mapping.items()}
+        return [[int(x/abs(x))*tmp_map[abs(x)] for x in clause] for clause in clauses]
+
+    #returns a tuple containing:
+    # an equivalent list of clauses with literals 1-n
+    # the mapping from old literals to new literals
+    @staticmethod
+    def rewriteFrom1toN(clauses):
+        usedLiterals = SATUtils.getUsedLiterals(clauses)
+        literalMapping = dict()
+        count = 1
+        for literal in usedLiterals:
+            literalMapping[abs(literal)] = count
+            count += 1
+        return([[int(x/abs(x))*literalMapping[abs(x)] for x in clause] for clause in clauses], literalMapping)
+
+    # returns the set of literals used in the clauses
+    @staticmethod
+    def getUsedLiterals(clauses):
+        usedLiterals = set()
+        for clause in clauses:
+            for literal in clause:
+                usedLiterals.add(abs(literal))
+        return usedLiterals
+
+    # given a set of literals and r, this produces a set of clauses that is
+    # only SAT when exactly r terms are true. Auxiliary literals start at
+    # 1+max(map(abs,literals)) or at the specified startLiteral
+    @staticmethod
+    def exactly(inLiterals, r, startLiteral = None):
+        geResult = SATUtils.atLeast(inLiterals, 2, startLiteral)
+        clauses = geResult[0]
+        ltResult = SATUtils.atMost(inLiterals, 2, geResult[1] + 1)
+        clauses = clauses + ltResult[0]
+        return (clauses, ltResult[1])
+
+    # given a set of literals and r, this produces a set of clauses that is
+    # only SAT when at least r terms are true. Auxiliary literals start at
+    # 1+max(map(abs,literals)) or at the specified startLiteral
+    @staticmethod
+    def atLeast(inLiterals, r, startLiteral = None):
+        inLiterals = [-x for x in inLiterals]
+        return SATUtils.atMost(inLiterals, len(inLiterals) - r, startLiteral)
+
+    # given a set of literals and r, this produces a set of clauses that is
+    # only SAT when at most r terms are true. Auxiliary literals start at
+    # 1+max(map(abs,literals)) or at the specified startLiteral
+    @staticmethod
+    def atMost(inLiterals, r, startLiteral = None):
+        if startLiteral == None:
+            startLiteral = max([abs(x) for x in inLiterals]) + 1
+        n=len(inLiterals)
+        clauses = []
+        # format:
+        # 2-tuple means use x[tuple[1]] with polarity tuple[0]
+        # 3-tuple means create variable tuple[0]*s^tuple[2]_tuple[1]
+
+        for k in range(1, r+1):
+            for j in range(1, n-r):
+                clauses.append([(-1, j, k), (1, j+1, k)])
+
+        for k in range(0, r+1):
+            for j in range(1, n-r+1):
+                if k == 0:
+                    clauses.append([(-1, j+k), (1, j, k+1)])
+                elif k == r:
+                    clauses.append([(-1, j+k), (-1, j, k)])
+                else:
+                    clauses.append([(-1, j+k), (-1, j, k), (1, j, k+1)])
+
+        # determine max j
+        maxSubscript = -sys.maxsize - 1
+        minSubscript = sys.maxsize
+        maxSuperscript = -sys.maxsize - 1
+        minSuperscript = sys.maxsize
+        for clause in clauses:
+            for literal in clause:
+                # only look at new variables
+                if len(literal) != 2:
+                    maxSubscript = literal[1] if literal[1] > maxSubscript else maxSubscript
+                    minSubscript = literal[1] if literal[1] < minSubscript else minSubscript
+                    maxSuperscript = literal[2] if literal[2] > maxSuperscript else maxSuperscript
+                    minSuperscript = literal[2] if literal[2] < minSuperscript else minSuperscript
+
+        # rewrite new variables from tuples to literals
+        for i in range(len(clauses)):
+            for j in range(len(clauses[i])):
+                # don't process x's yet, only handle s's
+                if len(clauses[i][j]) != 2:
+                                                #sign of literal
+                    clauses[i][j] = clauses[i][j][0]*( \
+                                    # Offset from input set of literals
+                                startLiteral + \
+                                    # Convert the subscript and superscripts into the index of a flattened
+                                    # 2d array. Adjusted so each dimension is 0-indexed
+                                (clauses[i][j][2]-minSuperscript)*(maxSubscript-minSubscript+1) + \
+                                (clauses[i][j][1]-minSubscript))
+                else:
+                    clauses[i][j] = clauses[i][j][0]*inLiterals[clauses[i][j][1]-1]
+        #print(maxSubscript)
+        largestLiteral = max([max([abs(x) for x in clause]) for clause in clauses])
+        return (clauses, largestLiteral)
+
+
+
 if __name__ == "__main__":
-    #solve('small_instances.txt')
-    n=5
+    n=10
     d=4
-    mg = McGregor(n=n, d=d, all=True)
+    mg = McGregor(n=n, d=d, all=False)
     if mg.solve():
         pp.pprint(mg.nodeDict)
         print(mg.verifyCorrectness())
-        print(len(mg.clauses))
-        print(mg.viewAssignments())
-        print(mg.assignments)
-        print(len(mg.allAssignments))
+        #print(len(mg.clauses))
+        #print(mg.viewAssignments())
+        #print(mg.assignments)
+        #print(len(mg.allAssignments))
     else:
         print('UNSAT')
+    # literals = [x+0 for x in [1, 2, 3, 4]]
+    # su = SATUtils.exactly(literals, 2, 1000)
+    # clauses, mapping = SATUtils.rewriteFrom1toN(su[0])
+    # pp.pprint(clauses)
+    # allSolutions = list(pycosat.itersolve(clauses+[[1], [-2], [-3] ]))
+    # pp.pprint(allSolutions)
+    # tmpSolutions = [SATUtils.applyLiteralMapping(clauses, mapping) for solution in allSolutions]
+    # print(tmpSolutions)
