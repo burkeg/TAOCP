a7f4c7e4cc52a6666ea8788a7c184d80a4f836b9
BURKE,GABE (K-SantaRosa,ex1) <gabe.burke@keysight.com>
2019-03-22 13:21:45 -0700

    work in progress, probably doesn't even compile... I'm hastily pushing this just so I can work from home

--- Volume 4/7.2.2.2/Witness.py
+++ Volume 4/7.2.2.2/Witness.py
@@ -16,6 +16,9 @@ class WitnessPuzzle:
         self.StartNodeNeighborConstraints = []
         self.EndNodeNeighborConstraints = []
         self.BaseConstraints = []
+        self.literalMapping = dict()
+        self.reverseLiteralMapping = dict()
+        self.auxLiteral = 0
         self.intepretPuzzle()
 
     def intepretPuzzle(self):
@@ -92,16 +95,20 @@ class WitnessPuzzle:
 
     def getStartNodeNeighborConstraints(self):
         startPoints = [x[0] for x in self.topology.items() if x[0][0] == 's']
-        self.StartNodeNeighborConstraints = [[(True, x) for x in startPoints]]
+        self.StartNodeNeighborConstraints = [[(1, x) for x in startPoints]]
         for startPoint in startPoints:
-            self.StartNodeNeighborConstraints.append([(True, x) for x in self.topology[startPoint]] + [(False, startPoint)])
+            self.StartNodeNeighborConstraints.append([(1, x) for x in self.topology[startPoint]] + [(-1, startPoint)])
         self.BaseConstraints += self.StartNodeNeighborConstraints
 
     def getEndNodeNeighborConstraints(self):
         endPoints = [x[0] for x in self.topology.items() if x[0][0] == 'e']
-        self.EndNodeNeighborConstraints = [[(True, x) for x in endPoints]]
+        self.EndNodeNeighborConstraints = [[(1, x) for x in endPoints]]
         for endPoint in endPoints:
-            self.EndNodeNeighborConstraints.append([(True, x) for x in self.topology[endPoint]] + [(False, endPoint)])
+            exactlyOneOf = [self.literalMapping[x] for x in self.topology[endPoint]]
+            oneOfClauses = SATUtils.exactlyOne(exactlyOneOf, self.auxLiteral)
+            self.auxLiteral = oneOfClauses[1]
+            self.clauses
+            self.EndNodeNeighborConstraints.append([(1, x) for x in self.topology[endPoint]] + [(-1, endPoint)])
         self.BaseConstraints += self.EndNodeNeighborConstraints
 
     def generatePuzzleTopology(self, symbolDict):
@@ -141,9 +148,42 @@ class WitnessPuzzle:
             nodesInClique += [('e', junction[0], junction[1])]
             cliqueDict = GraphColoring.generateKClique(nodesInClique)
             GraphColoring.mergeAintoB(cliqueDict, nodeDict)
+        for node in nodeDict.keys():
+            self.updateLiteralMapping(node)
         return nodeDict
 
+    def updateLiteralMapping(self, newSymbol):
+        if newSymbol not in self.literalMapping:
+            self.literalMapping[newSymbol] = self.auxLiteral + 1
+            self.reverseLiteralMapping[self.auxLiteral + 1] = newSymbol
+            self.auxLiteral += 1
 
+    def getSymbol(self, literal):
+        if literal not in self.reverseLiteralMapping:
+            if literal <= self.auxLiteral:
+                return ('aux', literal)
+            else:
+                raise Exception('Unknown literal (SAT)')
+        else:
+            tmp = self.reverseLiteralMapping[literal]
+            if literal > 0:
+                tmp[0] = 1
+            else:
+                tmp[0] = -1
+            return tmp
+
+    def getLiteral(self, symbol):
+        if symbol not in self.literalMapping:
+            raise Exception('Unknown symbol (SAT)')
+        else:
+            if symbol[0] == 'aux':
+                return symbol[1]
+            tmp = self.reverseLiteralMapping[symbol]
+            if symbol > 0:
+                tmp[0] = 1
+            else:
+                tmp[0] = -1
+            return tmp
 if __name__ == "__main__":
     # main_n = 15
     # main_r = 7
