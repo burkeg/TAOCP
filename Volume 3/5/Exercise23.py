9494c4b4e09f19b10335c56d58e735d6e6f81612
BURKE,GABE (K-SantaRosa,ex1) <gabe.burke@keysight.com>
2019-05-22 15:34:30 -0700

    Simplified approach to finding all k-cliques. Recusively find all k-1 cliques, then try to find a new node that shares neighbors with all other nodes in the clique to generate a k-clique. The base case is 2-cliques which are simply edges.

--- Volume 3/5/Exercise23.py
+++ Volume 3/5/Exercise23.py
@@ -6,8 +6,8 @@ import itertools
 import copy
 
 def solve():
-    numPeople = 30
-    acquaintencesPerPerson = 6
+    numPeople = 100
+    acquaintencesPerPerson = 30
     friendAdjList = [rand.sample([person for person in range(numPeople) if person != me], acquaintencesPerPerson) for me in range(numPeople)]
     friendAdjList = [set() for _ in range(numPeople)]
     for me in range(numPeople):
@@ -18,7 +18,7 @@ def solve():
             friendAdjList[me].add(otherPerson)
     friendAdjList = [[x for x in y] for y in friendAdjList]
     # friendAdjMatrix = [[1 if x in friendAdjList[curr] else 0 for x in range(numPeople)] for curr in range(numPeople)]
-    print(findAllKCliques(friendAdjList, 3))
+    print(findAllKCliques(friendAdjList, 4))
     print(sum([len(x) for x in friendAdjList])/len(friendAdjList))
     tmp = 0
     pass
@@ -26,18 +26,27 @@ def solve():
 def findAllKCliques(friendList, k):
     numPeople = len(friendList)
 
+    cliqueSet = set()
     # base case
-    if k == 3:
-        cliqueSet = set()
-        for curr in range(numPeople):
-            # sort rows so curr's neighbor is at the top
-            currNeighbors = sorted(friendList, key=lambda x: curr in x, reverse=True)
-            for neighborList in [friendList[x] for ]
-            while
+    if k == 2:
+        for person, friends in enumerate(friendList):
+            for friend in friends:
+                cliqueSet.add(frozenset([person, friend]))
+        return cliqueSet
         pass
     else:
         k_minus_one_cliques = findAllKCliques(friendList, k-1)
         # build up the list of k-cliques from k-1-cliques
+        for clique in k_minus_one_cliques:
+            sharedNodes = set(friendList[next(iter(clique))])
+            # for each node in the clique, see if there is a node that all clique members share
+            for neighbors in [set(friendList[x]) for x in clique]:
+                sharedNodes = sharedNodes.intersection(neighbors)
+            # for each node that is a neighbor with every other node in the clique, generate a new k-clique
+            for node in sharedNodes:
+                cliqueSet.add(frozenset(clique.union(set([node]))))
+        return cliqueSet
+
 
     tmp = 0
     pass
