9348d2864e783402d36e684c5fdddd4c9012243b
BURKE,GABE (K-SantaRosa,ex1) <gabe.burke@keysight.com>
2019-06-03 12:15:44 -0700

    Hooked up with sortUtils... I'll remove the absolute path don't worry :). Finished comparison sort

--- Volume 3/5.2/Algorithm C.py
+++ Volume 3/5.2/Algorithm C.py
@@ -1,11 +1,26 @@
 import sys
-sys.path.append(r'Volume 3/Sort Utils')
+sys.path.append(r'C:\Users\gabburke\Documents\Personal\TAOCP\Volume 3\Sort Utils')
 from SortUtils import SortUtils
 
+def reconstructFromInversionCount(arr, inversions):
+    sortedArr = []
+    for i in reversed(range(len(arr))):
+        sortedArr.insert(inversions[i], arr[i])
+    return sortedArr
 
 def ComparisonCounting(arr):
-    pass
+    counts = [0]*len(arr)
+    for i in range(len(arr) - 1):
+        for j in range(i+1, len(arr)):
+            if arr[i] > arr[j]:
+                counts[i] += 1
+    return reconstructFromInversionCount(arr, counts)
+
+def solve():
+    unsorted = SortUtils.randomIntegerArray(10, -4)
+    sortedArr = ComparisonCounting(unsorted)
+    print(unsorted)
+    print(sortedArr)
 
 if __name__ == '__main__':
-    data = 0
-    print(SortUtils.randomIntegerArray(10))
+    solve()
