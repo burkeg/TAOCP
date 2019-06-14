5a31b75fd8c1016e4b375a0a461a7053462f72eb
BURKE,GABE (K-SantaRosa,ex1) <gabe.burke@keysight.com>
2019-06-14 11:07:11 -0700

    Fixed algo C, finished algo D

---
+++ Volume 3/5.2/Algorithm_D.py
@@ -0,0 +1,38 @@
+import sys
+import copy
+sys.path.append(r'C:\Users\gabburke\Documents\Personal\TAOCP\Volume 3\Sort Utils')
+from SortUtils import SortUtils
+from Algorithm_C import ComparisonCounting
+
+def DistributionCounting(R):
+    K = copy.deepcopy(R) # Keys are equivalent to records in this case.
+    u = min(K)
+    v = max(K)
+    N = len(K)
+    counts = [0]*(v-u+1)    # D1
+    outputs = [None]*len(K)
+    indexer = lambda x: x-u
+    for j in range(N):      # D2
+        counts[indexer(K[j])]+= 1   # D3
+    # D4
+    for i in range(u+1, v+1):
+        counts[indexer(i)] = counts[indexer(i)] + counts[indexer(i-1)]
+    for j in reversed(range(len(K))):   # D5
+        i = counts[indexer(K[j])]
+        outputs[indexer(i)-1] = R[j]
+        counts[indexer(K[j])] = i - 1
+
+
+    return outputs
+
+def solve():
+    unsorted = SortUtils.randomIntegerArray(10)
+    # unsorted = [5,5,5,5,6,2,3,0]
+    sortedArr2 = ComparisonCounting(unsorted)
+    sortedArr = DistributionCounting(unsorted)
+    print(unsorted)
+    print(sortedArr)
+    print(sortedArr2)
+
+if __name__ == '__main__':
+    solve()
