a05f08d4c7cb3a3c0b445d0f7c7ee840612ee3a8
BURKE,GABE (K-SantaRosa,ex1) <gabe.burke@keysight.com>
2019-06-18 09:19:34 -0700

    more changes to Algo D

--- Volume 3/5.2/Algorithm_C.py
+++ Volume 3/5.2/Algorithm_C.py
@@ -18,9 +18,15 @@ def ComparisonCountingWeird(arr):
     return reconstructFromInversionCount(arr, counts)
 
 def ComparisonCounting(arr):
+    # C1
     counts = [0]*len(arr)
-    for i in range(len(arr) - 1):
-        for j in range(i+1, len(arr)):
+    # C2
+    # for i in reversed(range(len(arr) - 1)):
+    for i in range(1, len(arr)):
+        # C3
+        # for j in range(i+1, len(arr)):
+        for j in range(i):
+            # C4
             if arr[i] > arr[j]:
                 counts[i] += 1
             else:
@@ -33,10 +39,15 @@ def ComparisonCounting(arr):
 def solve():
     # unsorted = SortUtils.randomIntegerArray(10)
     # unsorted = SortUtils.KnuthExample()
-    unsorted = [rand.randint(50,60) for x in range(500)]
+    unsorted = [rand.randint(50,55) for x in range(20)]
     sortedArr = ComparisonCounting(unsorted)
-    print(unsorted)
-    print(sortedArr)
+    for a, b in zip(sortedArr, sorted(unsorted)):
+        if a != b:
+            break
+    else:
+        print('Matches:', sortedArr)
+        return
+    print("No match.", sortedArr)
 
 if __name__ == '__main__':
     solve()
--- Volume 3/5.2/Algorithm_D.py
+++ Volume 3/5.2/Algorithm_D.py
@@ -6,9 +6,10 @@ from SortUtils import SortUtils
 from Algorithm_C import ComparisonCounting
 
 def DistributionCounting(R, key=lambda x:x):
-    u = min(R)
-    v = max(R)
-    N = len(R)
+    origKeys = [key(x) for x in R]
+    u = min(origKeys)
+    v = max(origKeys)
+    N = len(origKeys)
     counts = [0]*(v-u+1)    # D1
     outputs = [None]*len(R)
     for j in range(N):      # D2
@@ -16,7 +17,7 @@ def DistributionCounting(R, key=lambda x:x):
     # D4
     for i in range(u+1, v+1):
         counts[i - u] = counts[i - u] + counts[i - 1 - u]
-    for j in reversed(range(len(R))):   # D5
+    for j in range(len(R)):   # D5
         i = counts[key(R[j]) - u]
         outputs[i-1] = R[j]
         counts[key(R[j]) - u] = i - 1
@@ -26,11 +27,13 @@ def DistributionCounting(R, key=lambda x:x):
 
 def solve():
     # unsorted = SortUtils.randomIntegerArray(1000)
-    unsorted = [rand.randint(50,55) for x in range(200000)]
+    # unsorted = [rand.randint(50,55) for x in range(200000)]
     # unsorted = [5,5,5,5,6,2,3,0]
     # unsorted = SortUtils.KnuthExample()
-    sortedArr = DistributionCounting(unsorted)
-    for a, b in zip(sortedArr, sorted(unsorted)):
+    keyFunc = lambda x:abs(x)
+    unsorted = list(reversed([x-10 for x in SortUtils.randomIntegerArray(20)]))
+    sortedArr = DistributionCounting(unsorted, keyFunc)
+    for a, b in zip(sortedArr, sorted(unsorted, key=keyFunc)):
         if a != b:
             break
     else:
